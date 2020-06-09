94cfc8443ff61a1c1f15bd63bce8c1dbbe840e69
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 15:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const driversArray=[1,2,3,4,5,11]
 const returnFirstTwoDrivers= function(driversArray){
-    return driversArray[0] + driversArray[1]
-  
+    newArray=[driversArray[0], driversArray[1] ] 
+  return newArray
 }
