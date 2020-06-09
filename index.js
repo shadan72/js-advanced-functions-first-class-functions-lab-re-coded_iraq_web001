da2f6f25765ea63986705b92ff74b8d8b3401336
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 15:47:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,4 +20,7 @@ const createFareMultiplier= function(integer){
 
 const fareDoubler = createFareMultiplier(2)
 const fareTripler=createFareMultiplier(3)
-const 
+const selectDifferentDrivers= function(){
+  
+}
+
