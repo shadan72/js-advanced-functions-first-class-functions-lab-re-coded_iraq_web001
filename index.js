77864736a9b945b39a013779932481a6dcaf8528
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 14:55:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers= function(){
-  return returnLastTwoDrivers(driversArray=[]){
+  return returnFirstTwoDrivers(driversArray=[]){
     
   }
 }
