4417ea022bb3ca6b2d7392ac4b46590b7eb4cba7
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 15:45:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const returnLastTwoDrivers= function(driversArray){
 const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
 
 const createFareMultiplier= function(integer){
-  return function(fare=1){
-  return fare *= interger
+  return function(fare){
+  return fare * interger
   }
 }
 
