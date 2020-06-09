77fceaebc8d53401f1047bc7caa69f31decc9ac2
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-09 15:13:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers= function(driversArray){
-    const newArray=[driversArray[0], driversArray[1]];
-  return newArray;
+    //const newArray=[driversArray[0], driversArray[1]];
+  //return newArray;
+  return driversArray.slice(0,2)
 };
